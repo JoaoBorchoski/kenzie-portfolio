48ecd615b8878c7053f7754803fd8af1647ce2a6
João Borchoski <128744140+JoaoBorchoski@users.noreply.github.com>
2023-05-17 13:38:29 -0300

    fix: linkedin account

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,7 +1,7 @@
 export const userData = {
     nameUser: "João Borchoski",
     githubUser: "JoaoBorchoski",
-    linkedinUser: "samuelleao",
+    linkedinUser: "joao-henrique-borchoski",
     whatsappNumber: "+5542991141279",
     emailUser: "joaoborchoskidev@gmail.com",
 };
